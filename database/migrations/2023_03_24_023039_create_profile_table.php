b545539f4a99935d0005cf297dfbe56630a92f0d
percyfikri <mafikri27@gmail.com>
2023-03-24 10:10:12 +0700

    feat(migration): add migration file

---
+++ database/migrations/2023_03_24_023039_create_profile_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama', 100)->index();
+            $table->string('nim')->index();
+            $table->string('kelas', 100)->index();
+            $table->string('absen')->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profile');
+    }
+};
