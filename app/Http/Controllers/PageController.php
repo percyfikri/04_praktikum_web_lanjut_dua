4f3c02ac99c9f309177cb9ea3554111eadeb84f9
percyfikri <mafikri27@gmail.com>
2023-03-24 14:24:18 +0700

    feat(views&controller): using data from database

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Experience;
+use App\Models\Profile;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -14,27 +16,29 @@ class PageController extends Controller
     }
 
     function profile(){
-        $data = [
-            'judul_profile' => 'Halaman Profile',
-            'tentang_saya' => 'Tentang Saya',
-            'profile' => [
-                'nama' => 'Muhammad Alvi Fikri',
-                'email' => 'alvifikri1@gmail.com',
-                'status' => 'Mahasiswa',
-                'kampus' => 'Jl. Soekarno Hatta No.9, Jatimulyo, Kec. Lowokwaru, Kota Malang, Jawa Timur 65141',
-                'alamat' => 'JL KH Hasyim Asyari, Kepanjen, Malang',
-                'skill1' => 'PHP',
-                'skill2' => 'Java'
-            ]
-            ];
-            return view('profile')->with($data);
+        $data = Profile::all();
+        return view('profile')->with('data', $data);
+
+
+        // $data = [
+        //     'judul_profile' => 'Halaman Profile',
+        //     'tentang_saya' => 'Tentang Saya',
+        //     'profile' => [
+        //         'nama' => 'Muhammad Alvi Fikri',
+        //         'email' => 'alvifikri1@gmail.com',
+        //         'status' => 'Mahasiswa',
+        //         'kampus' => 'Jl. Soekarno Hatta No.9, Jatimulyo, Kec. Lowokwaru, Kota Malang, Jawa Timur 65141',
+        //         'alamat' => 'JL KH Hasyim Asyari, Kepanjen, Malang',
+        //         'skill1' => 'PHP',
+        //         'skill2' => 'Java'
+        //     ]
+        //     ];
+        //     return view('profile')->with($data);
     }
 
     function experience(){
-        $data = [
-            'exp' => 'Pengalaman Kuliah'
-        ];
-        return view('experience')->with($data);
+        $data = Experience::all();
+        return view('experience')->with('data', $data);
     }
 
     function contactus(){
--- resources/views/experience.blade.php
+++ resources/views/experience.blade.php
@@ -10,23 +10,34 @@
         <div class="container-fluid">
           <div class="row">
             <div class="col-12">
+              @foreach ($data as $item)
+
               <!-- Default box -->
               <div class="card">
-                <div class="card-header">
-                  <h3 class="card-title">{{ $exp }}</h3>
-  
-                  <div class="card-tools">
-                    <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                      <i class="fas fa-minus"></i>
-                    </button>
+                  <div class="card-header">
+
+                      <h2 class="card-title">{{ $item->title }}</h2>
+                          <div class="card-tools">
+                              <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                                  <i class="fas fa-minus"></i>
+                              </button>
+                              <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+                                  <i class="fas fa-times"></i>
+                              </button>
+                          </div>
+                      </div>
+
+                      <div class="card-body">
+                          {{ $item->content }}
+                      </div>
+                      <!-- /.card-body -->
+                      <!-- /.card-footer-->
+
                   </div>
-                </div>
-                <div class="card-body">
-                    Sejak berkuliah di POLINEMA dengan memilih Jurusan Teknik Informasi Prodi D4 Teknik Informatika, Saya sangat senang sekali dikarenakan tugas yang diberikan oleh para dosen sangatlah mudah dan pemberian deadlinenya juga panjang. Oleh karena itu, saya sangat termotivasi untuk selalu rajin mengikuti perkuliahan dan mengerjakan tugas tepat waktu
-                </div>
-                <!-- /.card-body -->
-              </div>
-              <!-- /.card -->
+                  <!-- /.card -->
+
+              @endforeach
+
             </div>
           </div>
         </div>
