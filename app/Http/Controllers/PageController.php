9cf0826bf25ae3db44a56e2ed9f2137b933d6961
percyfikri <mafikri27@gmail.com>
2023-03-24 15:36:09 +0700

    little changes: delete comment

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -18,22 +18,6 @@ class PageController extends Controller
     function profile(){
         $data = Profile::all();
         return view('profile')->with('data', $data);
-
-
-        // $data = [
-        //     'judul_profile' => 'Halaman Profile',
-        //     'tentang_saya' => 'Tentang Saya',
-        //     'profile' => [
-        //         'nama' => 'Muhammad Alvi Fikri',
-        //         'email' => 'alvifikri1@gmail.com',
-        //         'status' => 'Mahasiswa',
-        //         'kampus' => 'Jl. Soekarno Hatta No.9, Jatimulyo, Kec. Lowokwaru, Kota Malang, Jawa Timur 65141',
-        //         'alamat' => 'JL KH Hasyim Asyari, Kepanjen, Malang',
-        //         'skill1' => 'PHP',
-        //         'skill2' => 'Java'
-        //     ]
-        //     ];
-        //     return view('profile')->with($data);
     }
 
     function experience(){
